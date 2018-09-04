f1a33ae21e4baf1f56f47aa72b2b475e30f5f7bf
dave <dave@beansmile.com>
2018-09-04 23:14:26 +0800

    feat: add core-js polyfill

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
+// for core-js polyfill
+const g = Function('return this')();
+g.parseInt = parseInt;
+g.parseFloat = parseFloat;
+g.Promise = Promise;
+g.Array = Array;
+g.Math = Math;
+g.Number = Number;
+g.Object = Object;
+g.Function = Function;
+g.String = String;
+g.RegExp = RegExp;
+g.Symbol = Symbol;
+
+require('core-js/modules/es7.promise.finally');
+require('core-js/modules/es7.symbol.async-iterator');
+
 App({});
