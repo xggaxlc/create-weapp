708868a56fcf011bf44465d9b379501fe624fe25
duwei <905571995@qq.com>
2018-09-10 21:53:43 +0800

    fix: build支持windows

--- scripts/plugin.js
+++ scripts/plugin.js
@@ -1,4 +1,4 @@
-const { resolve, join, dirname, relative } = require('path');
+const { resolve, dirname, relative } = require('path');
 const _ = require('lodash');
 const { ConcatSource } = require('webpack-sources');
 const NAME = 'WeappWebpackPlugin';
@@ -19,9 +19,10 @@ module.exports = class WeappWebpackPlugin {
 
     compiler.hooks.emit.tapAsync(NAME, (compilation, callback) => {
       const assets = compilation.assets;
+
       _.forEach(entryMap, (val, key) => {
         const assetsName = key.replace(appRoot, '').trim() + '.js';
-        const relativePath = relative(dirname(assetsName), `/${bundleName}`);
+        const relativePath = relative(dirname(assetsName), `/${bundleName}`).replace(/\\/g, '/');
         assets[assetsName] = new ConcatSource(`require('./${relativePath}').${val}();`);
       });
       callback();
